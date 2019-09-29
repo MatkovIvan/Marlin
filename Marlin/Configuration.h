792088d2b7d0194e026f52c73d8583e5224af8f6
Ivan Matkov <v-ivmatk@microsoft.com>
2019-09-29 15:31:52 +0300

    Adjust speed settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -474,9 +474,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Flying Bear Ghost 4
-  #define  DEFAULT_Kp 16.24
-  #define  DEFAULT_Ki 1.17
-  #define  DEFAULT_Kd 56.21
+  #define DEFAULT_Kp 10.82
+  #define DEFAULT_Ki 0.80
+  #define DEFAULT_Kd 36.40
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
@@ -732,7 +732,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 20, 40 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 30, 40 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -740,7 +740,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 150, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -750,8 +750,8 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          800     // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
+#define DEFAULT_ACCELERATION          1600    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
@@ -777,12 +777,12 @@
  * value set here, it may happen instantaneously.
  */
 #if DISABLED(JUNCTION_DEVIATION)
-  #define DEFAULT_XJERK 10.0
-  #define DEFAULT_YJERK 10.0
-  #define DEFAULT_ZJERK  0.3
+  #define DEFAULT_XJERK 20.0
+  #define DEFAULT_YJERK 20.0
+  #define DEFAULT_ZJERK  0.5
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK   10.0  // May be used by Linear Advance
 
 /**
  * S-Curve Acceleration
